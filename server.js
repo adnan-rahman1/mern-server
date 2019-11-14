24501870fa9cb715c11219e0d377beac0ea05286
Adnan Rahman <adnanibnamotin@yahoo.com>
2019-11-13 21:20:21 -0500

    add google auth functionality

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback"
+    }, (accessToken, refreshToken, profile, cb) => console.log(profile)
+));
+
+app.get("/", (req, res) => {
     res.send({
-        "hello": "world"
+        hello: "world"
     });
 });
+
+app.get("/auth/google", passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get(
+    "/auth/google/callback", 
+    passport.authenticate('google')
+);
 
 const port = process.env.PORT || 3000;
 
